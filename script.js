cfc9399e532fa61fe6a6b2f38d518c6efd2611d5
CsCheems <j.Dev.mx@outlook.com>
2025-06-11 12:08:23 -0600

    Se modifico el formato de fecha y hora

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function setTime(){
-    setInterval(
-        getTime, 1000
-    )
+function setTime() {
+    setInterval(getTime, 1000);
 }
 
 setTime();
 
-function getTime(){
+function getTime() {
     const main = document.getElementById("main-container");
+
     let timerDiv = document.getElementById('timer');
     if (!timerDiv) {
         timerDiv = document.createElement('div');
@@ -15,17 +14,29 @@ function getTime(){
         main.appendChild(timerDiv);
     }
 
+    let dateDiv = document.getElementById('dater');
+    if (!dateDiv) {
+        dateDiv = document.createElement('div');
+        dateDiv.id = 'dater';
+        main.appendChild(dateDiv);
+    }
+
     const currentTimeDate = new Date();
 
-    const formattedDate = currentTimeDate.toLocaleString('es-ES', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
+    const formattedTime = currentTimeDate.toLocaleTimeString('en-US', {
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit'
+        hour12: true
     });
 
-    timerDiv.innerHTML = formattedDate.toLocaleUpperCase();
+    const meses = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
+    const dia = currentTimeDate.getDate();
+    const mes = meses[currentTimeDate.getMonth()];
+    const anio = currentTimeDate.getFullYear();
+
+    const formattedDate = `${dia} ${mes} ${anio}`;
+
+    timerDiv.innerHTML = `${formattedTime}`;
+    dateDiv.innerHTML = `${formattedDate.toLocaleUpperCase()}`;
 }
+
